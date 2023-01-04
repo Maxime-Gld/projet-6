b3958e614ad66fb2f3db95c747e5837300b71b30
Maxime-Gld <Maxime.Gdev@hotmail.com>
2023-01-04 12:25:00 +0100

    first commit

---
+++ Back-end/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://Max:IcSJLpbSqqQGK3ON@cluster0.xtvojnj.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+app.use((req, res, next) => {
+    console.log('Requête reçue')
+    next();
+});
+
+app.use(cors({
+  origin: true
+}));
+
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+// test pour app
+/*
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+})
+
+app.use((req, res, next) => {
+    res.json({ message: 'Votre requête à bien été reçue'})
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Réponse envoyée avec succès !')
+});
+*/
+
+
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
---
+++ Back-end/routes/stuff.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const auth = require('auth');
+const router = express.Router();
+
+
