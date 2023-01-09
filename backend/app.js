bea4f2b8d00374c1210e7f9d88d469bd86343d3f
Maxime-Gld <Maxime.Gdev@hotmail.com>
2023-01-09 13:15:07 +0100

    ajout du path

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
 const cors = require('cors');
+const path = require('path');
+
+const app = express();
 
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
@@ -19,5 +21,6 @@ app.use(cors());
 
 app.use('/api/auth', userRoutes);
 app.use('/api/sauces', sauceRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
