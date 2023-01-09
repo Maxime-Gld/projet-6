22c74e0ab50b3d9ee671d344b409d95473d6cf05
Maxime-Gld <Maxime.Gdev@hotmail.com>
2023-01-09 13:18:07 +0100

    refonte du model

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator')
 
 const sauceSchema = mongoose.Schema({
-    name: { type: String, required: true, unique: true },
+    userId: { type: String, required: true },
+    name: { type: String, required: true },
     manufacturer: { type: String, required: true },
     description: { type: String, required: true },
-    heat: { type: Number, required: true },
     mainPepper: { type: String, required: true },
-    imageUrl: { type: String, required: true }
+    imageUrl: { type: String, required: true },
+    heat: { type: Number, required: true },
+    likes: { type: Number, default: 0 },
+    dislikes: { type: Number, default: 0 },
+    usersLiked: { type: [String] },
+    usersDisliked: { type: [String] }
 });
 
-sauceSchema.plugin(uniqueValidator);
-
 module.exports = mongoose.model('Sauce', sauceSchema);
+
