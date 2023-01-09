924f5e1ec0b5f8d2d1a93edffbce2cdbe3b13609
Maxime-Gld <Maxime.Gdev@hotmail.com>
2023-01-09 13:17:03 +0100

    ajout de multer dans les routes

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const auth = require('../middleware/auth');
 const router = express.Router();
 
-const sauceCtrl = require('../controllers/sauce')
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const sauceCtrl = require('../controllers/sauce');
 
 router.get('/', auth, sauceCtrl.getAllSauce);
-router.post('/', auth, sauceCtrl.createSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
-router.put('/:id', auth, sauceCtrl.modifySauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
 module.exports = router;
