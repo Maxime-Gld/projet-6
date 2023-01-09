31ecd3c2ab301c2ced387ca061e812146d2b5f04
Maxime-Gld <Maxime.Gdev@hotmail.com>
2023-01-09 13:19:14 +0100

    optimisation de get avec multer

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,77 +1,88 @@
 const Sauce = require('../models/Sauce');
 
+
 // the CRUD
 
 
 // method POST (Create)
 exports.createSauce = (req, res, next) => {
-    const Sauce = new Sauce({
-        name: req.body.name,
-        manufacterer: req.body.manufacturer,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        ingredient: req.body.ingredient,
-        heat: req.body.heat,
-        userId: req.body.userId
+    const sauceObject = JSON.parse(req.body.sauce);
+    delete sauceObject._id;
+    delete sauceObject._userId;
+    const sauce = new Sauce({
+        ...sauceObject,
+        userId: req.auth.userId,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
-    Sauce.save()
+
+    sauce.save()
     .then(() => { 
         res.status(201).json({
             message: 'Post saved successfully!'
-        });
+        })
     })
-    .catch(error => res.status(400).json({ error }));
+    .catch(error => {
+        res.status(400).json({ error })
+    })
 };
 
 
 // method GET only one (Read)
 exports.getOneSauce = (req, res, next) => {
-    Sauce.findOne({
-        _id: req.params.id
-    })
-    .then((sauce) => {
-        res.status(200).json(sauce);
-    })
-    .catch(error => res.status(404).json({ error }));
+    Sauce.findOne({ _id: req.params.id })
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(404).json({ error }))
 };
 
 
 // method PUT (Update)
 exports.modifySauce = (req, res, next) => {
-    const Sauce = new Sauce({
-        name: req.body.name,
-        manufacterer: req.body.manufacturer,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        ingredient: req.body.ingredient,
-        heat: req.body.heat,
-        userId: req.body.userId
-    });
-    Sauce.updateOne({_id: req.params.id}, sauce)
-    .then(() => {
-        res.status(201).json({
-            message: 'Sauce update successfully!'
-        });
-    })
-    .catch(error => res.status(400).json({ error }));
+    const sauceObject = req.file ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : {...req.body };
+
+    delete sauceObject.userId;
+    Sauce.findOne({_id: req.params.id})
+        .then((sauce) => {
+            if (sauce.userId != req.auth.userId) {
+                res.status(401).json({ message:"non-autorisé" })
+            } else {
+                sauce.updateOne({_id: req.params.id}, {...sauceObject, id: req.params.id})
+                .then(() => {
+                    res.status(201).json({
+                        message: 'Sauce update successfully!'
+                    })
+                })
+                .catch(error => { 
+                    res.status(400).json({ error })
+                })
+            }
+        })
 };
 
 
 // method Delete (Delete)
-exports.deleteSauce = (res, req, next) => {
+exports.deleteSauce = (req, res, next) => {
     Sauce.deleteOne({_id: req.params.id})
     .then(() => {
         res.status(200).json({
             message: 'Deleted'
-        });
+        })
     })
-    .catch(error => res.status(400).json({ error }));
+    .catch(error => { 
+        res.status(400).json({ error })
+    })
 };
 
 
 // affichage de toute les sauces
-exports.getAllSauce = (res, req, next) => {
+exports.getAllSauce = (req, res, next) => {
     Sauce.find()
-    .then(sauces => res.status(200).json(sauces))
-    .catch(error => res.status(400).json({ error }));
+    .then((sauces) => {
+        res.status(200).json(sauces)
+    })
+    .catch(error => {
+        res.status(400).json({ error })
+    })
 };
