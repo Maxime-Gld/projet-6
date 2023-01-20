508fb2c9e746965cd8545f93f8746187bc348f63
Maxime-Gld <Maxime.Gdev@hotmail.com>
2023-01-20 11:12:35 +0100

    validate.sauce et validate.id (joi)

--- backend/middleware/validate-input.js
+++ backend/middleware/validate-input.js
@@ -13,3 +13,38 @@ exports.user = (req, res, next) => {
         next();
     }
 };
+
+const sauceSchema = Joi.object({
+    userId: Joi.string().trim().length(24).required(),
+    name: Joi.string().trim().min(1).required(),
+    manufacturer: Joi.string().required(),
+    description: Joi.string().required(),
+    mainPepper: Joi.string().required(),
+    heat: Joi.number().integer().min(1).max(10).required()
+});
+
+exports.sauce = (req, res, next) => {
+   let sauce;
+    if (req.file) {
+        sauce = JSON.parse(req.body.sauce);
+    } else {
+        sauce = req.body;
+    }
+    const {error, value} = sauceSchema.validate(sauce);
+    if (error) {
+        res.status(422).json({ error: "vérifier les données saisie" });
+    } else {
+        next();
+    }
+};
+
+const idSchema = Joi.string().trim().length(24).required();
+
+exports.id = (req, res, next) => {
+    const {error, value} = idSchema.validate(req.params.id);
+    if (error) {
+        res.status(422).json({ error: "id invalide" });
+    } else {
+        next();
+    }
+};
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 const sauceCtrl = require('../controllers/sauce');
+const validate = require("../middleware/validate-input")
 
 router.get('/', auth, sauceCtrl.getAllSauce);
-router.post('/', auth, multer, sauceCtrl.createSauce);
-router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.post('/', auth, multer, validate.sauce, sauceCtrl.createSauce);
+router.get('/:id', auth, validate.id, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
-router.delete('/:id', auth, sauceCtrl.deleteSauce);
-router.post('/:id/like', auth, multer, sauceCtrl.likeSauce)
+router.delete('/:id', auth, multer, validate.id, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, validate.id, sauceCtrl.likeSauce)
 
 module.exports = router;
